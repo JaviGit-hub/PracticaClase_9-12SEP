6b57dde3a5fedcc4c82b57d2fbc2e5752f6f2148
JaviGit-hub <JAVIER.MARTINEZTORRES@ucr.ac.cr>
2026-09-12 22:06:25 -0600

    Proyecto completo Signed-off-by:JaviGit-hub <JAVIER.MARTINEZTORRES@ucr.ac.cr>

---
+++ src/main/java/main/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+/**
+ * Esta clase únicamente debe llamar a la clase Menu
+ * @author Javier MT
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        Menu.mostrarMenu();
+    }
+}
